ab87600a340845f0e8e54db8b0e451a9364c7426
Shankar <shankarkdhanaraj@gmail.com>
2018-10-15 00:55:41 -0400

    added signedintest endpoint

--- server/index.js
+++ server/index.js
@@ -10,7 +10,8 @@ const db = require('../database/dbUtils.js');
 const { logger } = require('./logger.js');
 
 const PORT = process.env.PORT || 50000;
-const API_GATEWAY_URL = process.env.API_GATEWAY_URL || 'http://localhost:9876';
+const API_ADMIN_GATEWAY_URL = process.env.API_ADMIN_GATEWAY_URL || 'http://localhost:9876';
+const API_GATEWAY_URL = process.env.API_GATEWAY_URL || 'http://localhost:50000';
 const HOME_PAGE_URL = process.env.HOME_PAGE_URL || 'http://18.217.151.202/homepage';
 const app = express();
 const jsonParser = bodyParser.json();
@@ -41,7 +42,7 @@ app.post('/createlogin', jsonParser, (req, res) => {
       // };
       console.log(jsonStringBody);
       const kongAPIGatewayOptionsCreateConsumer = {
-        url: `${API_GATEWAY_URL}/consumers`,
+        url: `${API_ADMIN_GATEWAY_URL}/consumers`,
         json: true,
         method: 'POST',
         'User-Agent': 'request',
@@ -77,8 +78,9 @@ app.get('/signingin', jsonParser, (req, res) => {
         res.send(JSON.stringify({ userId: null })); // NEEDS TO BE CHANGED LATER
       } else {
         // CHECK FOR PASSWORD HERE
+        console.log(`testing URL....${API_ADMIN_GATEWAY_URL}`);
         const kongAPIGatewayOptionsCreateCredential = {
-          url: `${API_GATEWAY_URL}/consumers/${userId}/jwt`,
+          url: `${API_ADMIN_GATEWAY_URL}/consumers/${userId}/jwt`,
           method: 'POST',
           'User-Agent': 'request',
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -89,10 +91,16 @@ app.get('/signingin', jsonParser, (req, res) => {
           'User-Agent': 'request',
           'Access-Control-Allow-Origin': true,
         };
+        const kongAPIGatewayOptionsSignedInTest = {
+          method: 'GET',
+          'User-Agent': 'request',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Access-Control-Allow-Origin': true,
+        };
 
         // DELETE ANY EXISTING CREDENTIAL
         // curl -X GET http://kong:8001/consumers/{consumer}/jwt
-        fetch(`${API_GATEWAY_URL}/consumers/${userId}/jwt`)
+        fetch(`${API_ADMIN_GATEWAY_URL}/consumers/${userId}/jwt`)
           .then(response => response.json())
           .then((credentialList) => {
             if (credentialList.total > 1) {
@@ -100,66 +108,42 @@ app.get('/signingin', jsonParser, (req, res) => {
               res.send({ error: `error: too many credentials for the user, ${userId}` });
               return 1;
             } else if (credentialList.total === 1) {
-              const deletUrl = `${API_GATEWAY_URL}/consumers/${userId}/jwt/${credentialList.data[0].id}`;
+              const deletUrl = `${API_ADMIN_GATEWAY_URL}/consumers/${userId}/jwt/${credentialList.data[0].id}`;
               console.log(`deleting credential...${deletUrl}`);
               return fetch(deletUrl, kongAPIGatewayOptionsDeleteCredential);
             }
             return 2;
           })
-          .then(() => {
-            // CREATE THE KONG CREDENTIALS FOR THE USER WITH FETCH
-            fetch(`${API_GATEWAY_URL}/consumers/${userId}/jwt`, kongAPIGatewayOptionsCreateCredential)
-              .then(response => response.json())
-              .then((credential) => {
-                console.log('Created Credential is...', credential);
-                const {
-                  id: credentialId,
-                  secret,
-                  key,
-                } = credential;
-                // CRETATE THE JWT WITH THE CREDENTIALS FROM KONG
-                const token = createJWT(userId, credentialId, secret, key);
-                console.log('created token is...', token);
+          .then(() => fetch(`${API_ADMIN_GATEWAY_URL}/consumers/${userId}/jwt`, kongAPIGatewayOptionsCreateCredential))
+          .then(response => response.json())
+          .then((credential) => {
+            console.log('Created Credential is...', credential);
+            const {
+              id: credentialId,
+              secret,
+              key,
+            } = credential;
+            // CRETATE THE JWT WITH THE CREDENTIALS FROM KONG
+            const token = createJWT(userId, credentialId, secret, key);
+            console.log('created token is...', token);
 
-                // SEND THE JWT BACK
-                res.json({ token });
-              });
-            // // CREATE THE KONG CREDENTIALS FOR THE USER WITH REQUEST - DONE
-            // request(kongAPIGatewayOptionsCreateCredential, (err, response, body) => {
-            //   if (err) {
-            //     console.log('error in create credential call', err);
-            //   } else {
-            //     console.log('Created Credential is...', body);
-            //     const {
-            //       id: credentialId,
-            //       secret,
-            //       key,
-            //     } = JSON.parse(body);
-            //     // CRETATE THE JWT WITH THE CREDENTIALS FROM KONG
-            //     const token = createJWT(userId, credentialId, secret, key);
-            //     console.log('created token is...', token);
-
-            //     // SEND THE JWT BACK
-            //     res.json({ token });
-            //   }
-            // });
+            // SEND THE JWT BACK
+            // res.json({ token });
+            return token;
+          })
+          .then(token => fetch(`${API_GATEWAY_URL}/signedintest?jwt=${token}`))
+          .then(response => response.text())
+          .then((text) => {
+            console.log(text);
+            res.send(text);
           });
-
-        // const url = `${HOME_PAGE_URL}?userid=${userId}`;
-        // console.log('Redirected url is...', url);
-        // // res.status(200).send(`<html><body></body><script type="text/javascript">window.location.href=${url};</script></html>`);
-        // // res.status(301);
-        // // return res.redirect(url);
-        // return res.redirect(301, url);
-        // res.send(JSON.stringify({ userId }));
       }
     });
 });
 
 app.get('/signedintest', jsonParser, (req, res) => {
-  console.log('Request is..', req);
-  logger.info({ 'requested user': req.body });
-  res.send('you are now signed in');
+  logger.info({ 'received token at /signedintest is': req.query.token });
+  res.send('you are now signed into /signedintest');
 });
 
 app.get('/signedinhome', jsonParser, (req, res) => {
